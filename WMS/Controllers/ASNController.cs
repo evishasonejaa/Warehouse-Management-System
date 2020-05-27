654fd9d9a8968e81eea214d18a495405dcbc303b
surekhakolekar9 <surekhak@zconsolutions.com>
2020-05-27 15:19:35 +0530

    Code commit to upload local repo.

---
+++ WMS/Controllers/ASNController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WMS.Controllers
+{
+    public class ASNController : Controller
+    {
+         
+        private WMSEntities db = new WMSEntities();
+        // GET: S
+        public ActionResult Index()
+        {
+            IEnumerable<tbl_asn> students = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:50020/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("ASN");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<tbl_asn>>();
+                    readTask.Wait();
+
+                    students = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    students = Enumerable.Empty<tbl_asn>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(students);
+        }
+
+        public ActionResult Create()
+        {
+                ViewBag.client_name = new SelectList(db.tbl_client, "client_name", "client_name");
+            return View();
+        }
+
+      
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(tbl_asn student)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:50020/api/ASN");
+
+                //HTTP POST
+                var postTask = client.PostAsJsonAsync<tbl_asn>("ASN", student);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+
+            return View(student);
+        }
+        public ActionResult Edit(int id)
+        {
+            tbl_asn student = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:50020/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("ASN?id=" + id.ToString());
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<tbl_asn>();
+                    readTask.Wait();
+
+                    student = readTask.Result;
+                }
+            }
+            ViewBag.client_name = new SelectList(db.tbl_client, "client_name", "client_name");
+            return View(student);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(tbl_asn student)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:50020/api/ASN");
+
+                //HTTP POST
+                var putTask = client.PutAsJsonAsync<tbl_asn>("ASN", student);
+                putTask.Wait();
+
+                var result = putTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(student);
+        }
+        public ActionResult Delete(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:50020/api/");
+
+                //HTTP DELETE
+                var deleteTask = client.DeleteAsync("ASN/" + id.ToString());
+                deleteTask.Wait();
+
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
+
---
+++ WebApi/Controllers/ASNController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi;
+
+namespace WebApi.Controllers
+{
+    public class ASNController : ApiController
+    {
+        private WMS_dbEntities1 db = new WMS_dbEntities1();
+
+        // GET: api/ASN
+        public IQueryable<tbl_asn> Gettbl_asn()
+        {
+            return db.tbl_asn;
+        }
+
+        // GET: api/ASN/5
+        [ResponseType(typeof(tbl_asn))]
+        public IHttpActionResult Gettbl_asn(int id)
+        {
+            tbl_asn tbl_asn = db.tbl_asn.Find(id);
+            if (tbl_asn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tbl_asn);
+        }
+
+        // PUT: api/ASN/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Puttbl_asn(int id, tbl_asn tbl_asn)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != tbl_asn.order)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(tbl_asn).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!tbl_asnExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ASN
+        [ResponseType(typeof(tbl_asn))]
+        public IHttpActionResult Posttbl_asn(tbl_asn tbl_asn)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.tbl_asn.Add(tbl_asn);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (tbl_asnExists(tbl_asn.order))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = tbl_asn.order }, tbl_asn);
+        }
+
+        // DELETE: api/ASN/5
+        [ResponseType(typeof(tbl_asn))]
+        public IHttpActionResult Deletetbl_asn(int id)
+        {
+            tbl_asn tbl_asn = db.tbl_asn.Find(id);
+            if (tbl_asn == null)
+            {
+                return NotFound();
+            }
+
+            db.tbl_asn.Remove(tbl_asn);
+            db.SaveChanges();
+
+            return Ok(tbl_asn);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool tbl_asnExists(int id)
+        {
+            return db.tbl_asn.Count(e => e.order == id) > 0;
+        }
+    }
+}
